7b6d2b9e4baf23279bf7ba6adc832dc15d88ea74
Walid <wbaya.p@proton.me>
2025-08-07 03:04:49 +0100

    add error handling and logs + refactoring

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ USER appuser
 ENTRYPOINT ["python", "main.py"]
 
 #> docker build -t book_scraper:v1 ./
-#> docker run -it --rm --init --security-opt seccomp=seccomp_profile.json -v ./app/:/home/appuser/app/ books_scraper:v1
+#> docker run -it --rm --init --security-opt seccomp=seccomp_profile.json -v ./app/:/home/appuser/app/ book_scraper:v1
--- app/main.py
+++ app/main.py
@@ -1,74 +1,94 @@
 from playwright.sync_api import sync_playwright
+import logging as log
+
+log.basicConfig(level=log.DEBUG, format="[%(asctime)s - %(levelname)s]\t%(message)s")
+
+error_log = log.getLogger("error")
+error_handler = log.FileHandler("logs/error.log", mode="w")
+error_log.addHandler(error_handler)
+
+network_log = log.getLogger("network")
+network_handler = log.FileHandler("logs/network.log", mode="w")
+network_log.addHandler(network_handler)
 
 with sync_playwright() as p:
-
     browser = p.firefox.launch()
     context = browser.new_context()
     page = context.new_page()
+    page.on("request", lambda req: network_log.info(f"[REQ] {req.method} {req.url}"))
+    page.on("response", lambda res: network_log.info(f"[RES] {res.status} {res.url}"))
 
-    page.goto("https://books.toscrape.com/")
-    print("On the landing page")
-    page.screenshot(path="screenshots/00-landing_page.png")
+    try:
+        step = "main"
+        network_log.info(f"<{step}>")
+        page.goto("https://books.toscrape.com/")
+        log.debug(f"</{step}>")
 
-    page.get_by_role("link", name="Travel", exact=True).click()
-    print("<Travel> Clicked")
-    page.screenshot(path="screenshots/01-travel.png")
-
-    book_info = {
-            "upc": [],
-            "title": [],
-            "price_excl_tax": [],
-            "price_incl_tax": [],
-            "stars": [],
-            "num_reviews": [],
-            "availability": []
-            }
-    books_articles = page.locator("ol.row li")
-    books_articles_count = books_articles.count()
-    print(f"The number of books on <Travel> is: {books_articles_count}")
-    for nth in range(books_articles_count):
-        books_articles.nth(nth).locator("h3 a").click()
-        print(f"Book {nth + 1} Clicked")
-        page.screenshot(path=f"screenshots/{(nth + 2):>02}-book{nth + 1}.png")
-
-        title = page.locator(".product_main h1").inner_text()
-        book_info["title"].append(title)
-        print(f"\ttitle: {title}")
-
-        stars_class = page.locator(".product_main p.star-rating").get_attribute("class")
-        stars = stars_class.split()[1]
-        book_info["stars"].append(stars)
-        print(f"\tstarts: {stars}")
-
-        info_table = page.locator(".table-striped")
-
-        upc_tr = info_table.locator("tr").filter(has_text="UPC")
-        upc = upc_tr.locator("td").inner_text()
-        book_info["upc"].append(upc)
-        print(f"\tupc: {book_info["upc"][nth]}")
-
-        price_excl_tax_tr = info_table.locator("tr").filter(has_text="Price (excl. tax)")
-        price_excl_tax = price_excl_tax_tr.locator("td").inner_text()
-        book_info["price_excl_tax"].append(price_excl_tax)
-        print(f"\tprice_excl_tax: {book_info["price_excl_tax"][nth]}")
-
-        price_incl_tax_tr = info_table.locator("tr").filter(has_text="Price (incl. tax)")
-        price_incl_tax = price_incl_tax_tr.locator("td").inner_text()
-        book_info["price_incl_tax"].append(price_incl_tax)
-        print(f"\tprice_incl_tax: {book_info["price_incl_tax"][nth]}")
-
-        availability_tr = info_table.locator("tr").filter(has_text="Availability")
-        availability = availability_tr.locator("td").inner_text()
-        book_info["availability"].append(availability)
-        print(f"\tavailability: {book_info["availability"][nth]}")
-
-        num_reviews_tr = info_table.locator("tr").filter(has_text="Number of reviews")
-        num_reviews = num_reviews_tr.locator("td").inner_text()
-        book_info["num_reviews"].append(num_reviews)
-        print(f"\tnum_reviews: {book_info["num_reviews"][nth]}")
-
+        step = "travel"
+        network_log.info(f"<{step}>")
         page.get_by_role("link", name="Travel", exact=True).click()
-        print("Back To <Travel>")
+        log.debug(f"</{step}>")
 
-    print(book_info)
+        books_loc = page.locator("ol.row li")
+        books_count = books_loc.count()
+        log.debug(f"books count: {books_count}")
+
+        info_table = None
+        def get_info_table_data(text, table = info_table):
+            data_tr = info_table.locator("tr").filter(has_text=text)
+            data = data_tr.locator("td").inner_text()
+            return data
+
+        books_info = []
+
+        for n in range(books_count):
+            step = f"book{(n + 1):>02}"
+            network_log.info(f"<{step}>")
+            books_loc.nth(n).locator("h3 a").click()
+            log.debug(f"</{step}>")
+
+            title = page.locator(".product_main h1").inner_text()
+            log.debug(f"\ttitle: {title}")
+
+            stars_class = page.locator(".product_main p.star-rating").get_attribute("class")
+            stars = stars_class.split()[1]
+            log.debug(f"\tstars: {stars}")
+
+            info_table = page.locator(".table-striped")
+
+            upc = get_info_table_data("UPC")
+            log.debug(f"\tupc: {upc}")
+
+            price_excl_tax = get_info_table_data("Price (excl. tax)")
+            log.debug(f"\tprice_excl_tax: {price_excl_tax}")
+
+            price_incl_tax = get_info_table_data("Price (incl. tax)")
+            log.debug(f"\tprice_incl_tax: {price_incl_tax}")
+
+            availability = get_info_table_data("Availability")
+            log.debug(f"\tavailability: {availability}")
+
+            num_reviews = get_info_table_data("Number of reviews")
+            log.debug(f"\tnum_reviews: {num_reviews}")
+
+            books_info.append({
+                "upc"           : upc,
+                "title"         : title,
+                "availability"  : availability,
+                "price_excl_tax": price_excl_tax,
+                "price_incl_tax": price_incl_tax,
+                "stars"         : stars,
+                "num_review"    : num_reviews
+            })
+
+            step = "travel"
+            network_log.info(f"<{step}>")
+            page.go_back()
+            log.debug(f"</{step}>")
+
+        print(books_info)
+
+    except Exception as e:
+        error_log.exception(step)
+        page.screenshot(path=f"logs/[ERROR]-{step}.png", full_page=True)
 
